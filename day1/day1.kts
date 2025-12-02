ee115d4d4c9c2e1ab771b641b4c602b3bb95a308
pierre <mail@pierremackay.com>
2025-12-01 21:59:24 -0500

    part 2

--- day1/day1.kts
+++ day1/day1.kts
@@ -9,34 +9,33 @@ var rotateLetter = "R"
 var rotationMovement = 0
 
 fun determinePosition(currentPosition: Int, rotationSide: String, rotationMovement: Int): Int {
-    var newPosition: Int? = null
     if (rotationSide == "R") {
-        newPosition = currentPosition + rotationMovement
+        return (currentPosition + rotationMovement) % 100
     } else {
-        newPosition = currentPosition - rotationMovement
+        val result = (currentPosition - rotationMovement) % 100
+        return if (result < 0) result + 100 else result
     }
-    return overUnder100(newPosition)
 }
 
 fun roundDownToNearest100(number: Int): Int {
     return (number / 100) * 100
 }
 
-fun detemineRevolution(nearest100: Int) {
-    var movement = 0
-    if (rotateLetter == "L") {
-        movement = currentValue - rotationMovement
-        if (movement < 0) {
-            if (currentValue != 0 && 100 - abs(movement) < 100) {
-                zeroRevolutionCount += 1
-            }
-        }
-    } else if (rotateLetter == "R") {
-        movement = currentValue + rotationMovement
-        if (movement > 100) {
-            zeroRevolutionCount += 1
-        }
+fun detemineRevolution() {
+    val newPosition = determinePosition(currentValue, rotateLetter, rotationMovement)
+    val firstHit = if (rotateLetter == "R") {
+        if (currentValue == 0) 100 else 100 - currentValue
+    } else {
+        if (currentValue == 0) 100 else currentValue
     }
+    var revolutions = 0
+    if (rotationMovement >= firstHit) {
+        revolutions = 1 + (rotationMovement - firstHit) / 100
+    }
+    if (newPosition == 0 && revolutions > 0) {
+        revolutions -= 1
+    }
+    zeroRevolutionCount += revolutions
 }
 
 fun overUnder100(number: Int): Int {
@@ -44,22 +43,20 @@ fun overUnder100(number: Int): Int {
     if (number > 99) {
         var nearest100 = roundDownToNearest100(number)
         determinedPosition = number - nearest100
-        detemineRevolution(nearest100)
         return determinedPosition
     } else if (number < 0) {
         var nearest100 = roundDownToNearest100(abs(number))
         if (nearest100 == 0) {
             nearest100 = 100
         }
-        detemineRevolution(nearest100)
         return nearest100 - abs(number)
     } else {
         return number
     }
 }
 
-fun part1() {
-    val filePath = "/home/pierre/Documents/Coding/aoc-2025/day1/test_input.txt"
+fun part1_and_2() {
+    val filePath = "/home/pierre/Documents/Coding/aoc-2025/day1/input.txt"
     try {
         val lines = File(filePath).readLines()
         lines.forEachIndexed { index, line ->
@@ -70,18 +67,19 @@ fun part1() {
                 rotateLetter = "L"
             }
             rotationMovement = lineArray[1].toInt()
-            var newValue = determinePosition(currentValue, rotateLetter, rotationMovement)
+            detemineRevolution()
+            val newValue = determinePosition(currentValue, rotateLetter, rotationMovement)
             currentValue = newValue
             if (currentValue == 0) {
                 zeroCount = zeroCount + 1
             }
         }
         println("Part 1: " + zeroCount)
-        var part2 = zeroCount + zeroRevolutionCount
+        val part2 = zeroCount + zeroRevolutionCount
         println("Part 2: " + part2)
     } catch (e: Exception) {
         println("Error reading file: ${e.message}")
     }
 }
 
-part1()
+part1_and_2()
